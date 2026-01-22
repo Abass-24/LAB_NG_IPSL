458ef01981be81f4d03eac4de00a8808164dfd2d
Zeydia <douseydiallo2002@gmail.com>
2026-01-22 12:01:08 +0000

    affichage des produits

--- src/app/pages/produit-list/produit-list.ts
+++ src/app/pages/produit-list/produit-list.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../../models/produit.model';
+import { ProduitService } from '../../services/produit-service.service';
+import { ProduitItemDiv } from '../produit-item-div/produit-item-div';
 
 @Component({
   selector: 'app-produit-list',
-  imports: [],
+  imports: [ProduitItemDiv],
   templateUrl: './produit-list.html',
   styleUrl: './produit-list.scss',
 })
-export class ProduitList {
+export class ProduitList implements OnInit{
   
+  produits: Produit[] = [];
+
+
+  constructor(private produitService: ProduitService) {  }
+
+  ngOnInit(): void {
+    this.loadProduits();
+  }
+
+  loadProduits(){
+    this.produitService.getAll().subscribe({
+      next:(data: Produit[]) => {
+        this.produits = data;
+      } 
+    });
+  }
+
+
 }
